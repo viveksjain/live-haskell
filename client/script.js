9e01b937dc8c134ab108608d845a308dd329d4a2
Vivek Jain <vivek@vivekja.in>
2016-03-08 11:18:08 -0800

    Remember filename in browser

--- client/script.js
+++ client/script.js
@@ -7,12 +7,19 @@ $(document).ready(function () {
 
 function FileSelector(live_haskell) {
   var that = this;
+  if (localStorage.filename) {
+    $('#form_filename').val(localStorage.filename);
+  }
   $('#file_selector').submit(function(ev) {
     ev.preventDefault();
 
+    var filename = $('#form_filename').val();
     // TODO
-    // $.post('open', {filename: $('#form_filename').val()}, function (result) {
+    // $.post('open', {filename: filename}, function (result) {
     // });
+
+    // On success
+    localStorage.filename = filename;
     var contents = "-- Type here and it will get evaluated when you press enter (careful, make sure\n-- you don't execute any potentially dangerous code!)\nmain' :: String\nmain' = \"Hello world\"\ntest = map (+)";
     live_haskell.setInput(contents);
     live_haskell.enable(that);
