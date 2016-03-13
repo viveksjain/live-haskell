a7b98a7ec05430082a49a7a8ffe7ba42e43738fa
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-03-13 14:24:43 -0700

    Fix SafePrelude export list

--- server/lib/SafePrelude.hs
+++ server/lib/SafePrelude.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE Safe #-}
 
-module SafePrelude(module Prelude, TIO, runTIO, IO, readFile, writeFile, appendFile, hiddenDummyMain) where
+module SafePrelude(module Prelude, TIO, runTIO, IO, readFile, writeFile, appendFile, hiddenDummyMain, putChar, putStr, putStrLn, print, getChar, getLine, getContents, interact, readIO, readLn) where
 
 import qualified System.IO as SIO (IO)
-import Prelude hiding (IO, readFile, writeFile, appendFile, putChar, putStr, putStrLn, print, getChar, getLine, getLine, getContents, interact, readIO, readLn)
+import Prelude hiding (IO, readFile, writeFile, appendFile, putChar, putStr, putStrLn, print, getChar, getLine, getContents, interact, readIO, readLn)
 
-import System.TIO(TIO, runTIO, readFile, writeFile, appendFile, putChar, putStr, putStrLn, print, getChar, getLine, getLine, getContents, interact, readIO, readLn)
+import System.TIO(TIO, runTIO, readFile, writeFile, appendFile, putChar, putStr, putStrLn, print, getChar, getLine, getContents, interact, readIO, readLn)
 
 type IO = TIO
 
